2479b0534f68a2fc28f6fdacd5d2d7457f55c2b9
dimzachar <zacharenakis@proton.me>
2024-07-05 16:31:47 +0300

    Add 02-open-source cohort

---
+++ cohorts/2024/02-open-source/energy.py
@@ -0,0 +1,18 @@
+
+from openai import OpenAI
+
+client = OpenAI(
+    base_url='http://localhost:11434/v1/',
+    api_key='ollama',
+)
+
+prompt = "What's the formula for energy?"
+response = client.chat.completions.create(
+    model='gemma:2b',
+    messages=[{"role": "user", "content": prompt}],
+    temperature=0.0
+)
+print(response.choices[0].message.content)
+
+completion_tokens = response.usage.completion_tokens
+print(f"Number of completion tokens: {completion_tokens}")
